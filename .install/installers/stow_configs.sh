dc2b89fe760ca5e8a7479aebb75a91c90f05d6ce
vignesh-g-05 <vicky2805vky@gmail.com>
2026-04-22 23:49:58 +0530

    feat(stow): add clearer stow logs

--- .install/installers/stow_configs.sh
+++ .install/installers/stow_configs.sh
@@ -3,35 +3,45 @@ STOWED_PACKAGES=()
 CONFLICTING_PACKAGES=()
 
 stow_config () {
-  if run_command stow --target="$HOME" --simulate "$1"; then
-    if ! $DRY_RUN; then
-      stow --target="$HOME" "$1"
+    if run_command stow --target="$HOME" --simulate "$1"; then
+        
+        if $DRY_RUN; then
+            log_info "[dry-run] would stow $1"
+            WOULD_STOW_PACKAGES+=("$1")
+            return 0
+        fi
+        
+        if run_command stow --target="$HOME" "$1"; then
+            log_success "stowed $1 successfully"
+            STOWED_PACKAGES+=("$1")
+        else
+            log_error "failed to stow $1 (real run)"
+            CONFLICTING_PACKAGES+=("$1")
+        fi
+        
+    else
+        log_error "failed to stow $1 (simulation)"
+        CONFLICTING_PACKAGES+=("$1")
     fi
-
-    STOWED_PACKAGES+=("$1") 
-  else
-    log_error "failed to stow $1"
-    CONFLICTING_PACKAGES+=("$1")
-  fi
 }
 
 stow_configs () {
-
-  if ! command -v stow >/dev/null 2>&1; then
-    log_error "GNU stow is not installed"
-    return 1
-  fi
-  
-  if $VERBOSE; then
-    log_info "stowing packages"
-  else
-    log_loading "stowing packages"
-  fi
-
-  for pkg in "${STOW_PACKAGES[@]}"; do
-    stow_config "$pkg"
-  done
-
-  log_info "stowing completed"
-  show_stow_summary
+    
+    if ! command -v stow >/dev/null 2>&1; then
+        log_error "GNU stow is not installed"
+        return 1
+    fi
+    
+    if $VERBOSE; then
+        log_info "stowing packages"
+    else
+        log_loading "stowing packages"
+    fi
+    
+    for pkg in "${STOW_PACKAGES[@]}"; do
+        stow_config "$pkg"
+    done
+    
+    log_info "stowing completed"
+    show_stow_summary
 }
